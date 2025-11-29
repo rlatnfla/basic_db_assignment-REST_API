501ab75295dc736b67eb935608c6adfb02f6b330
rlatnfla <rlatnfla0724@gmail.com>
2025-11-29 16:04:11 +0900

    init: initial commit

---
+++ src/test/java/com/example/basic_db_rest_api/BasicDbRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basic_db_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicDbRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
