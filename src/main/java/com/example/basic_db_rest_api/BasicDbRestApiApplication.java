ba172c9966637a23e3ca08f6ab67edf8b258d175
rlatnfla <rlatnfla0724@gmail.com>
2025-11-29 18:35:33 +0900

    feat: 애플리케이션 실행 시, html 파일 파싱 및 스키마 초기화, 데이터 삽입

--- src/main/java/com/example/basic_db_rest_api/BasicDbRestApiApplication.java
+++ src/main/java/com/example/basic_db_rest_api/BasicDbRestApiApplication.java
@@ -1,13 +1,33 @@
 package com.example.basic_db_rest_api;
 
+import com.example.basic_db_rest_api.init.InitializeManager;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BasicDbRestApiApplication {
+@Slf4j
+public class BasicDbRestApiApplication implements CommandLineRunner {
 
+    private final InitializeManager initializeManager;
+
+    public BasicDbRestApiApplication(InitializeManager initializeManager) {
+        this.initializeManager = initializeManager;
+    }
     public static void main(String[] args) {
         SpringApplication.run(BasicDbRestApiApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        try {
+
+            initializeManager.init();
+
+        } catch (Exception e) {
+
+            log.error("애플리케이션 초기화 중 오류 발생: {}", e.getMessage());
+        }
+    }
 }
